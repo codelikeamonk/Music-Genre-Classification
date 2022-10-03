7286f8ce7ed305b69c73ebc1870d3e910d5cb597
codelikeamonk <92156468+codelikeamonk@users.noreply.github.com>
2022-10-03 18:50:27 +0530

    Create vector.py

---
+++ vector.py
@@ -0,0 +1,10 @@
+def getNeighbors(trainingSet, instance, k):
+    distances = []
+    for x in range (len(trainingSet)):
+        dist = distance(trainingSet[x], instance, k )+ distance(instance, trainingSet[x], k)
+        distances.append((trainingSet[x][2], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
